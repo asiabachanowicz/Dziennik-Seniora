cf100f80cb0422d64d2ffe9fc23e850edd677b92
kingatan7 <kinga-tanska@wp.pl>
2020-04-14 21:59:25 +0200

    Initial commit
    Add login view and send data with login and password
    to server.
    
    Signed-off-by: kingatan7 <kinga-tanska@wp.pl>

--- app/src/main/java/com/example/dziennikseniora/FirstFragment.java
+++ app/src/main/java/com/example/dziennikseniora/FirstFragment.java
@@ -1,13 +1,20 @@
 package com.example.dziennikseniora;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class FirstFragment extends Fragment {
 
@@ -23,12 +30,34 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+        TextView logintext = view.findViewById(R.id.textView);
+        logintext.setText("Wpisz swój login");
+        TextView passtext = view.findViewById(R.id.textView2);
+        passtext.setText("Wpisz swoje hasło");
+
+        final EditText loginedit = view.findViewById(R.id.editText3);
+        final EditText passedit = view.findViewById(R.id.editText4);
+
+        Button loginbutt = view.findViewById(R.id.button);
+
+
+        loginbutt.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
+            public void onClick(View v) {
+                JSONObject logindata = new JSONObject();
+                String user_login = loginedit.getText().toString();
+                String user_pass = passedit.getText().toString();
+                if (user_login != "" && user_pass != "") {
+                    try {
+                        logindata.put("login", user_login);
+                        logindata.put("password", user_pass);
+                        Log.e("TAG", logindata.toString());
+                        new SendJSONtoServer().execute("http://192.168.0.66:8080/telematyka-serwer/servletdata", logindata.toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                }
         });
     }
 }
