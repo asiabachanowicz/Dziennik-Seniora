2bfda3f3a9b7cb300959728c5947af0d462dbe7b
kingatan7 <kinga-tanska@wp.pl>
2020-06-03 21:43:33 +0200

    Getting doctor message and send own. Initial GUI doctor's message
    Signed-off-by: kingatan7 <kinga-tanska@wp.pl>

--- app/src/main/java/com/example/dziennikseniora/DoctorMessage.java
+++ app/src/main/java/com/example/dziennikseniora/DoctorMessage.java
@@ -1,11 +1,15 @@
 package com.example.dziennikseniora;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,23 +24,56 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class DoctorMessage extends AppCompatActivity {
     TextView doctor_msg;
+    EditText send_msg;
+    Button send_btn;
+    String strValue;
+    static final JSONObject logindata = new JSONObject();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
-        String server_url = "http://192.168.0.66:8080/telematyka-serwer/wiadomosc";
-        JSONObject logindata = new JSONObject();
+        final String server_url = "http://192.168.0.66:8080/telematyka-serwer/wiadomosc";
+        final EditText simpleEditText = (EditText) findViewById(R.id.editText7);
+        final TextView logdate = findViewById(R.id.timeView);
+        TextView date;
+        Date currentTime = Calendar.getInstance().getTime();
+        date = (TextView) findViewById(R.id.timeView);
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        date.setText(dateFormatter.format(currentTime));
         try {
             logindata.put("login", FirstFragment.user_login);
+            logindata.put("tresc", null);
+            logindata.put("date", null);
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
         new DoctorMessage.RaportOperation().execute(server_url, logindata.toString());
+        send_btn = findViewById(R.id.button3);
+        send_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                strValue = simpleEditText.getText().toString();
+                String user_logDate = logdate.getText().toString();
+                try {
+                    logindata.put("tresc", strValue);
+                    logindata.put("date", user_logDate);
+                    new DoctorMessage.RaportOperation().execute(server_url, logindata.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                new DoctorMessage.RaportOperation().execute(server_url, logindata.toString());
+            }
+        });
     }
     private class RaportOperation extends AsyncTask<String, Void, String> {
 
@@ -103,6 +140,7 @@ public class DoctorMessage extends AppCompatActivity {
             String msg = result.substring(29);
             doctor_msg = findViewById(R.id.textView7);
             doctor_msg.setText(msg);
+            DoctorMessage.logindata.remove("tresc");
         }
     }
 }
